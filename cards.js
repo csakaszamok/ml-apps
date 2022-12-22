4eb237cbfeecfc84d553e881670a89d865ad2441
csakaszamok <csakaszamok@gmail.com>
2022-12-22 16:53:56 +0100

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,27 @@
+export const cards =
+    [
+        {
+            "title": "Fungi",
+            "subtitle": "Fungi classifier",
+            "category": "Survivor",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.",
+            "image": "mushrooms",
+            "link": "link2.html"
+        },
+        {
+            "title": "Dogs",
+            "subtitle": "Dog breed classifier",
+            "category": "Fun",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.",
+            "image": "dogs",
+            "link": "link2.html"
+        },
+        {
+            "title": "Skin",
+            "subtitle": "Skin disases classifier",
+            "category": "Health",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.",
+            "image": "skin,disasese",
+            "link": "link2.html"
+        },
+    ]
